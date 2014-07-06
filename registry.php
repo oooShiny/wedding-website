5fe2bcd4b859e38699503b7450bcc625f36ce0ca
Andrew Brown <ABrown@genuineinteractive.com>
2014-07-06 14:30:00 -0400

    Registry icon changes, adding text below icons.

--- registry.php
+++ registry.php
@@ -44,9 +44,9 @@
 
 				<div id="registry" class="pageContent">
 					<p>
-					<a href="https://www.nearbyregistry.com/index.cfm/go/shop.registry-display-grid?reg=141" target="_blank"><img src="images/flagNearby.png" /></a>
-            <a href="http://www.crateandbarrel.com/Gift-Registry/Erin-Marchal-and-Andrew-Brown/r5123757" target="_blank"><img src="images/flagCrate.png" /></a>
-            <a href="http://www.potterybarn.com/registry/3499485/registry-list.html" target="_blank"><img src="images/flagPottery.png" /></a>
+          <h2><a href="https://www.nearbyregistry.com/index.cfm/go/shop.registry-display-grid?reg=141" target="_blank"><img src="images/registry-nearby.png" /><br><br>Nearby Registry</a></h2>
+            <h2><a href="http://www.crateandbarrel.com/Gift-Registry/Erin-Marchal-and-Andrew-Brown/r5123757" target="_blank"><img src="images/registry-cb.png" /><br><br>Crate &amp; Barrel</a></h2>
+          <h2><a href="http://www.potterybarn.com/registry/3499485/registry-list.html" target="_blank"><img src="images/registry-pb.png" /><br><br>Pottery Barn</a></h2>
 					</p>
 				</div>
 			</div>
